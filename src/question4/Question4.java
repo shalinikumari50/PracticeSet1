846f02617cf35d878efc929f6a38d0ebb777777d
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-19 15:09:26 +0530

    add if-else to check number is positive,negative or zero

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -6,6 +6,14 @@ public class Question4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+
 
         scanner.close();
     }
