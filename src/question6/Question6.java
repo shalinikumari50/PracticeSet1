748c0e8e16263ee5dd7d23afb354f0f20b68bfb7
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-19 15:28:55 +0530

    add if-else to arrange alphabetically

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -7,6 +7,11 @@ public class Question6 {
         Scanner scanner = new Scanner(System.in);
         char value1 = scanner.nextLine().trim().charAt(0);
         char value2 = scanner.nextLine().trim().charAt(0);
+        if (value1 > value2) {
+            System.out.println(value2 + " " + value1);
+        } else {
+            System.out.println(value1 + " " + value2);
+        }
 
         scanner.close();
 
