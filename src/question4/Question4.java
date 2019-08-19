e5530f8aebaf9ae04d7f9b3ae2005a7c9a08fbb5
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-19 14:58:13 +0530

    add object:scanner for class:Scanner

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -1,4 +1,12 @@
 package question4;
 
+import java.util.Scanner;
+
 public class Question4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        scanner.close();
+    }
 }
