301c08279c2a5e12a1fdd32b5b10ec1341cda0c9
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-19 15:14:39 +0530

    add if-else to check odd-even

--- src/question5/Question5.java
+++ src/question5/Question5.java
@@ -6,6 +6,12 @@ public class Question5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+
 
         scanner.close();
     }
