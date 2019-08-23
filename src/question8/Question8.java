649628213ad49dfaaa3628a72fe79be1c6e0a031
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-23 06:03:52 +0530

    add input for gender(String) and age(int)

--- src/question8/Question8.java
+++ src/question8/Question8.java
@@ -1,4 +1,15 @@
 package question8;
 
+import java.util.Scanner;
+
 public class Question8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String gender = scanner.nextLine().trim();
+        int age = scanner.nextInt();
+
+
+        scanner.close();
+    }
+
 }
