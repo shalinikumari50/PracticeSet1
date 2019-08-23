38f8870faf2f22746be2ca5bedef537e7240b6c1
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-23 05:57:11 +0530

    add if-else to check for digit,alphabet or special character

--- src/question7/Question7.java
+++ src/question7/Question7.java
@@ -6,6 +6,13 @@ public class Question7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char value = scanner.nextLine().trim().charAt(0);
+        if (Character.isLetter(value)) {
+            System.out.println("alphabet");
+        } else if (Character.isDigit(value)) {
+            System.out.println("digit");
+        } else {
+            System.out.println("special character");
+        }
 
         scanner.close();
     }
