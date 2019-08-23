950b7e7d770f93c8c68973771a23f481c126a4c1
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-23 06:16:55 +0530

    add if-else for the different conditions

--- src/question8/Question8.java
+++ src/question8/Question8.java
@@ -5,9 +5,22 @@ import java.util.Scanner;
 public class Question8 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String gender = scanner.nextLine().trim();
+        String gender = scanner.next().trim();
         int age = scanner.nextInt();
-
+        if (gender.equals("female")) {
+            if (age >= 1 && age <= 58) {
+                System.out.println("8.2%");
+            } else if (age >= 59 && age <= 100) {
+                System.out.println("9.2%");
+            }
+        }
+        if (gender.equals("male")) {
+            if (age >= 1 && age <= 58) {
+                System.out.println("8.4%");
+            } else if (age >= 59 && age <= 100) {
+                System.out.println("10.5%");
+            }
+        }
 
         scanner.close();
     }
