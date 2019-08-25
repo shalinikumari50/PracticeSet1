17ec24df9d969c86258e958a81a9356634f82985
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-25 11:14:43 +0530

    add for loop and if-else to check prime number

--- src/question12/Question12.java
+++ src/question12/Question12.java
@@ -7,6 +7,18 @@ public class Question12 {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         int c = 1;
+        for (int i = 2; i < (int) (Math.sqrt(number)) + 1; i++) {
+            if (number % i == 0) {
+                c = 0;
+                break;
+            }
+
+        }
+        if (number == 1 || c == 0) {
+            System.out.println(number + " is not a prime number");
+        } else {
+            System.out.println(number + " is a prime number");
+        }
 
         scanner.close();
     }
