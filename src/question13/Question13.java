4123f14064e77025540ff41af31301b0d08d2c35
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-25 11:27:03 +0530

    add while loop to calculate sumOfDigits

--- src/question13/Question13.java
+++ src/question13/Question13.java
@@ -6,6 +6,13 @@ public class Question13 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
+        int sumOfDigits = 0;
+        while (number > 0) {
+            sumOfDigits = sumOfDigits + number % 10;
+            number /= 10;
+
+        }
+        System.out.println(sumOfDigits);
         scanner.close();
     }
 }
