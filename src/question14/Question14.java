78df130ea51c6695e6caeb2c1e4cf6586ea53989
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-25 11:31:30 +0530

    add object:scanner of class:Scanner input variable number(int)

--- src/question14/Question14.java
+++ src/question14/Question14.java
@@ -1,4 +1,11 @@
 package question14;
 
+import java.util.Scanner;
+
 public class Question14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        scanner.close();
+    }
 }
