5502d7f8b1343b098b590c4905e0f4ec13e060b5
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-25 10:14:25 +0530

    add object:scanner of class:Scanner add variable monthNumber(int)

--- src/question10/Question10.java
+++ src/question10/Question10.java
@@ -1,4 +1,12 @@
 package question10;
 
+import java.util.Scanner;
+
 public class Question10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int monthNumber = scanner.nextInt();
+
+        scanner.close();
+    }
 }
