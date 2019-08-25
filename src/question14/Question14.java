33eda20caaf1d28f6f187e35b82598d6795710e2
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-25 11:43:29 +0530

    add while and if-else to check palindrome

--- src/question14/Question14.java
+++ src/question14/Question14.java
@@ -6,6 +6,19 @@ public class Question14 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
+        int copyOfNum = number;
+        int reverse = 0;
+        int digit;
+        while (number > 0) {
+            digit = number % 10;
+            reverse = reverse * 10 + digit;
+            number /= 10;
+        }
+        if (copyOfNum == reverse) {
+            System.out.println(copyOfNum + " is a palindrome");
+        } else {
+            System.out.println(copyOfNum + " is not a palindrome");
+        }
         scanner.close();
     }
 }
