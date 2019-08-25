95427a8d09702a0a3042eec1e0f1938c39f54cb7
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-25 10:27:08 +0530

    add object:scanner of class:Scanner add variables number1(int) and number2(int)

--- src/question11/Question11.java
+++ src/question11/Question11.java
@@ -1,4 +1,14 @@
 package question11;
 
+import java.util.Scanner;
+
 public class Question11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+
+
+        scanner.close();
+    }
 }
