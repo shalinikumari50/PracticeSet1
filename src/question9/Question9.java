fa200d2540efd6c06fe9e054c8d84c33ef7a3ead
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2019-08-25 09:51:30 +0530

    add if-else to check Case do the required conversion

--- src/question9/Question9.java
+++ src/question9/Question9.java
@@ -6,6 +6,11 @@ public class Question9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         char value = scanner.nextLine().trim().charAt(0);
+        if (Character.isLowerCase(value)) {
+            System.out.println(Character.toUpperCase(value));
+        } else {
+            System.out.println(Character.toLowerCase(value));
+        }
 
         scanner.close();
     }
